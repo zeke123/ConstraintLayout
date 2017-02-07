f258e8b94fbdd45222445fbc71014d72246b490c
zhoujian <1064791247@qq.com>
2017-02-07 14:42:45 +0800

    zeke  提交代码

--- app/src/main/java/com/zhoujian/constraintlayout/MainActivity.java
+++ app/src/main/java/com/zhoujian/constraintlayout/MainActivity.java
@@ -3,7 +3,8 @@ package com.zhoujian.constraintlayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
